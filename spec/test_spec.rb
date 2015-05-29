ed95fba9910a651f9c170c4922c0029ab6967d0c
Samoilenko Yuri <kinnalru@gmail.com>
2015-05-29 21:55:07 +0300

    work is not acomplished

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -16,13 +16,13 @@ BASEREPO="#{TMPDIR}/base"
 # DECRYPTREPO="#{TMPDIR}/decrypt"
 
 FILE1="file1"
-DATA1="123\n321\n555\nabcdef\we must make some diff context\nqweqwe\nasdasd\nzxczxc\nrtyfghvbn\nhjkhjk"
+DATA1="123\n321\n555\nabcdef\n We must make some diff context for testing \nqweqwe\nasdasd\nzxczxc\nrtyfghvbn\nhjkhjk"
 
 FILE2="file2"
 DATA2="asd\nefg\nzxc\nqwerty"
 
 def clone_repo base, path
-  Dir.chdir TMPDIR
+  Dir.chdir "#{base}/../"
 
   path = %x(
     git clone #{base} #{path}
@@ -37,218 +37,222 @@ end
 
 NAMES={cr: 'crypted', cr1: 'crypted_1', re: 'recrypted', cr2: 'crypted_2', dc1: 'decrypted'}
 
+PASS_TYPES=[
+  [:gpg, "gpg\n\n\n\n#{FILE1}\n"],
+  [:password, "mypassword\n\n\n\n#{FILE1}\n"]
+]
 
-PASS_TYPES=["gpg\n\n\n\n#{FILE1}\n", "mypassword\n\n\n\n#{FILE1}\n"]
-
-  describe num + "Testing gpg features" do
-
-      before(:all) do
-        @initstr = "gpg\n\n\n\n#{FILE1}\n"
-        @baserepo = "#{TMPDIR}/base"
-      end
-
-
-      it num + "creating base repo" do
-          %x(
-            mkdir -p #{BASEREPO}
-            pushd #{BASEREPO}
-            git init
-            > .initial
-            git add .initial
-            git commit -m "initial"
-            echo -e "#{DATA1}" >> #{FILE1}
-            echo -e "#{DATA2}" >> #{FILE2}
-            git add "#{FILE1}" "#{FILE2}"
-            git commit -am "commit"
-          )
-          expect(File.read(BASEREPO+"/"+FILE1).strip).to eq DATA1
-          expect(File.read(BASEREPO+"/"+FILE2).strip).to eq DATA2
-      end
-
-      it num + "creating crypted repo [#{NAMES[:cr]}]" do
-          @repos[:cr] = clone_repo BASEREPO, NAMES[:cr] do |repo|
-            Dir.chdir repo
-            expect(File.read(FILE1).strip).to eq DATA1
-            expect(File.read(FILE2).strip).to eq DATA2
-
-            puts %x(
-                echo "#{ptype}" | gitcrypt init
-                rm #{FILE1}
-                git reset --hard
-            )
-            expect(%x(git config gitcrypt.key).strip.length).to eq 32
-            expect(File.read(FILE1).strip).to eq ""
-            expect(File.read(FILE2).strip).to eq DATA2
-          end
-
-          Dir.chdir TMPDIR
-          %x(
-            pushd #{TMPDIR} && rm -rf *
-          )
-      end
-
-      # it num + "crypting repo [#{NAMES[:cr]}] history" do
-      #     Dir.chdir @repos[:cr]
-      #     %x(
-      #         echo "y\n" | gitcrypt crypthistory
-      #     )
-      #     expect(File.read(FILE1).strip).to eq DATA1
-      #     expect(File.read(FILE2).strip).to eq DATA2
-      # end
+$passcounter = 0
+PASS_TYPES.each do |type_p|
+  describe "Testing #{type_p[0]} features" do
+    before(:all) do
+      @type = PASS_TYPES[$passcounter][0]
+      @initstr = PASS_TYPES[$passcounter][1]
+      @repos = {}
+      `mkdir -p "#{TMPDIR}/#{@type}"`
       
-      # it num + "cloning crypted repo [#{NAMES[:cr]}] to [#{NAMES[:cr1]}]" do
+      @baserepo = "#{TMPDIR}/#{@type}/base"
+      $passcounter += 1
+    end
 
-      #     @repos[:cr1] = clone_repo @repos[:cr], NAMES[:cr1] do |repo|
-      #       Dir.chdir repo
-      #       expect(File.read(FILE1).strip).to_not eq DATA1
-      #       expect(File.read(FILE2).strip).to eq DATA2
+    before(:each) do
+      Dir.chdir "#{TMPDIR}/#{@type}"
+    end
 
-      #       %x(
-      #           echo "#{ptype}" | gitcrypt init
-      #           rm #{FILE1}
-      #           git reset --hard
-      #       )
+    it "creating base repo" do
+      %x(
+        mkdir -p #{@baserepo}
+        pushd #{@baserepo}
+        git init
+        > .initial
+        git add .initial
+        git commit -m "initial"
+        echo -e "#{DATA1}" >> #{FILE1}
+        echo -e "#{DATA2}" >> #{FILE2}
+        git add "#{FILE1}" "#{FILE2}"
+        git commit -am "commit"
+      )
+      Dir.chdir @baserepo
+      expect(File.read(FILE1).strip).to eq DATA1
+      expect(File.read(FILE2).strip).to eq DATA2
+    end
 
-      #       expect(File.read(FILE1).strip).to eq DATA1
-      #       expect(File.read(FILE2).strip).to eq DATA2
-      #     end
+    it "creating crypted repo [#{NAMES[:cr]}]" do
+      @repos[:cr] = clone_repo @baserepo, NAMES[:cr] do |repo|
+        Dir.chdir repo
+        expect(File.read(FILE1).strip).to eq DATA1
+        expect(File.read(FILE2).strip).to eq DATA2
 
-      # end
+        %x(
+          echo "#{@initstr}" | gitcrypt init
+          gitcrypt reset
+        )
 
-      # it num + "recrypting repo [#{NAMES[:cr1]}]" do
-      #     Dir.chdir @repos[:cr1]
-      #     newpass = "newpass"
-      #     %x(
-      #         echo "\n#{newpass}\n" | gitcrypt recrypt
-      #     )
+        if @type == :password
+          expect(%x(git config gitcrypt.pass).strip).to eq ""
+        else
+          expect(%x(git config gitcrypt.pass).strip).to eq "gpg"
+        end
 
-      #     @repos[:re] = clone_repo @repos[:cr1], NAMES[:re] do |repo|
-      #       Dir.chdir repo
-      #       expect(File.read(FILE1).strip).to_not eq DATA1
-      #       expect(File.read(FILE2).strip).to eq DATA2
+        expect(%x(git config gitcrypt.secret).strip.length).to eq 32
+        expect(File.read(FILE1).strip).to eq ""
+        expect(File.read(FILE2).strip).to eq DATA2
+      end
+    end
 
-      #       %x(
-      #           echo "#{ptype}" | gitcrypt init
-      #           git reset --hard
-      #       )
+    it "crypting repo [#{NAMES[:cr]}] history" do
+      Dir.chdir @repos[:cr]
+      %x(
+        echo "y\n" | gitcrypt crypthistory
+      )
+      expect(File.read(FILE1).strip).to eq DATA1
+      expect(File.read(FILE2).strip).to eq DATA2
+    end
+    
+    it "cloning crypted repo [#{NAMES[:cr]}] to [#{NAMES[:cr1]}]" do
+      @repos[:cr1] = clone_repo @repos[:cr], NAMES[:cr1] do |repo|
+        Dir.chdir repo
+        expect(File.read(FILE1).strip).to_not eq DATA1
+        expect(File.read(FILE2).strip).to eq DATA2
 
-      #       expect(%x(git config gitcrypt.pass).strip).to eq newpass
-      #       expect(File.read(FILE1).strip).to eq DATA1
-      #       expect(File.read(FILE2).strip).to eq DATA2
-      #     end
-          
-      #     Dir.chdir TMPDIR
-      #     %x(
-      #       pushd #{TMPDIR} && rm -rf *
-      #     )
+        %x(
+          echo "#{@initstr}" | gitcrypt init
+          gitcrypt reset
+        )
 
-      # end
+        expect(File.read(FILE1).strip).to eq DATA1
+        expect(File.read(FILE2).strip).to eq DATA2
+      end
+    end
 
-      # it "decrypting repo [#{NAMES[:cr1]}]" do
-      #     Dir.chdir @repos[:cr1]
-      #     %x(
-      #         gitcrypt decrypthistory
-      #     )
+    it "disable/enable gytcrypt facility in repo [#{NAMES[:cr]}]" do
+      Dir.chdir @repos[:cr]
+      expect(File.read(FILE1).strip).to eq DATA1
+      expect(File.read(FILE2).strip).to eq DATA2 
 
-      #     @repos[:dc1] = clone_repo @repos[:cr1], NAMES[:dc1] do |repo|
-      #       Dir.chdir repo
-      #       expect(File.read(FILE1).strip).to eq DATA1
-      #       expect(File.read(FILE2).strip).to eq DATA2
-      #     end
-          
+      %x(
+        gitcrypt disable
+        gitcrypt reset
+      )
 
-      # end
+      expect(File.read(FILE1).strip).to_not eq DATA1
+      expect(File.read(FILE2).strip).to eq DATA2 
 
-      # it "merging crypted repos [#{NAMES[:cr]}] and [#{NAMES[:cr2]}]" do
-    		# NEWDATA1_PREF = "NEWDATA\n#{DATA1}"
-    		# NEWDATA1_SUFF = "#{DATA1}\nNEWDATA"
+      %x(
+        gitcrypt enable
+        gitcrypt reset
+      )
 
-      #   @repos[:cr2] = clone_repo @repos[:cr], NAMES[:cr2] do |repo|
-      #     Dir.chdir repo
-      #     %x(
-      #         echo "#{ptype}" | gitcrypt init
-      #         git reset --hard
-      #     ) 
-      #     expect(File.read(FILE1).strip).to eq DATA1
-      #     expect(File.read(FILE2).strip).to eq DATA2
-      #   end
+      expect(File.read(FILE1).strip).to eq DATA1
+      expect(File.read(FILE2).strip).to eq DATA2 
 
-      #   Dir.chdir @repos[:cr]
-      #   %x(
-      #     echo -e "#{NEWDATA1_PREF}" > #{FILE1}
-      #     git add "#{FILE1}"
-      #     git commit -am "commit for merge 1"
-      #   )
-
-      #   Dir.chdir @repos[:cr2]
-      #   %x(
-      #     echo -e "#{NEWDATA1_SUFF}" > #{FILE1}
-      #     git add "#{FILE1}"
-      #     git commit -am "commit for merge 2"
-      #   )
-
-      #   expect(File.read(@repos[:cr]+"/"+FILE1).strip).to eq NEWDATA1_PREF
-      #   expect(File.read(@repos[:cr]+"/"+FILE2).strip).to eq DATA2
-
-      #   expect(File.read(@repos[:cr2]+"/"+FILE1).strip).to eq NEWDATA1_SUFF
-      #   expect(File.read(@repos[:cr2]+"/"+FILE2).strip).to eq DATA2
-
-      #   Dir.chdir @repos[:cr2]
-
-      #   %x(
-      #     git pull
-      #   )
-
-      #   expect(File.read(FILE1).strip).to eq "NEWDATA\n#{DATA1}\nNEWDATA"
-
-      # end
+    end
 
 
-      # it "merging CONFLICTED crypted repos [#{NAMES[:cr]}] and [#{NAMES[:cr2]}]" do
-      #   NEWDATA1_PREF = "CONFLICTED\n#{DATA1}"
-      #   NEWDATA1_SUFF = "ANOTHER_CONFLICTED#{DATA1}"
+    it "decrypting repo [#{NAMES[:cr1]}]" do
+      Dir.chdir @repos[:cr1]
+      %x(
+        gitcrypt decrypthistory
+      )
 
-      #   Dir.chdir @repos[:cr]
-      #   %x(
-      #     echo -e "#{NEWDATA1_PREF}" > #{FILE1}
-      #     git add "#{FILE1}"
-      #     git commit -am "commit for conflict 1"
-      #   )
+      @repos[:dc1] = clone_repo @repos[:cr1], NAMES[:dc1] do |repo|
+        Dir.chdir repo
+        expect(File.read(FILE1).strip).to eq DATA1
+        expect(File.read(FILE2).strip).to eq DATA2
+      end
+    end
 
-      #   Dir.chdir @repos[:cr2]
-      #   %x(
-      #     echo -e "#{NEWDATA1_SUFF}" > #{FILE1}
-      #     git add "#{FILE1}"
-      #     git commit -am "commit for conflict 2"
-      #   )
 
-      #   expect(File.read(@repos[:cr]+"/"+FILE1).strip).to eq NEWDATA1_PREF
-      #   expect(File.read(@repos[:cr]+"/"+FILE2).strip).to eq DATA2
+    it "merging crypted repos [#{NAMES[:cr]}] and [#{NAMES[:cr2]}]" do
+  		NEWDATA1_PREF = "NEWDATA\n#{DATA1}"
+  		NEWDATA1_SUFF = "#{DATA1}\nNEWDATA"
 
-      #   expect(File.read(@repos[:cr2]+"/"+FILE1).strip).to eq NEWDATA1_SUFF
-      #   expect(File.read(@repos[:cr2]+"/"+FILE2).strip).to eq DATA2
+      @repos[:cr2] = clone_repo @repos[:cr], NAMES[:cr2] do |repo|
+        Dir.chdir repo
+        %x(
+          echo "#{@initstr}" | gitcrypt init
+          git reset --hard
+        ) 
+        expect(File.read(FILE1).strip).to eq DATA1
+        expect(File.read(FILE2).strip).to eq DATA2
+      end
 
-      #   Dir.chdir @repos[:cr2]
+      Dir.chdir @repos[:cr]
+      %x(
+        echo -e "#{NEWDATA1_PREF}" > #{FILE1}
+        git add "#{FILE1}"
+        git commit -am "commit for merge 1"
+      )
 
-      #   %x(
-      #     git pull
-      #   )
+      Dir.chdir @repos[:cr2]
+      %x(
+        echo -e "#{NEWDATA1_SUFF}" > #{FILE1}
+        git add "#{FILE1}"
+        git commit -am "commit for merge 2"
+      )
 
-      #   conflict = %x(
-      #     git status | grep "both modified"
-      #     echo $?
-      #   ).split("\n").last.strip == "0"
+      expect(File.read(@repos[:cr]+"/"+FILE1).strip).to eq NEWDATA1_PREF
+      expect(File.read(@repos[:cr]+"/"+FILE2).strip).to eq DATA2
 
-      #   expect(conflict).to eq true
+      expect(File.read(@repos[:cr2]+"/"+FILE1).strip).to eq NEWDATA1_SUFF
+      expect(File.read(@repos[:cr2]+"/"+FILE2).strip).to eq DATA2
 
-      #   %x(
-      #     git checkout --theirs file1
-      #     git add file1
-      #     git commit -m "take other changes"
-      #   )
+      Dir.chdir @repos[:cr2]
 
-      #   expect(File.read(@repos[:cr2]+"/"+FILE1).strip).to eq File.read(@repos[:cr]+"/"+FILE1).strip
-      # end
+      %x(
+        git pull
+      )
 
+      expect(File.read(FILE1).strip).to eq "NEWDATA\n#{DATA1}\nNEWDATA"
+    end
+
+
+    it "merging CONFLICTED crypted repos [#{NAMES[:cr]}] and [#{NAMES[:cr2]}]" do
+      NEWDATA1_PREF_C = "CONFLICTED\n#{DATA1}"
+      NEWDATA1_SUFF_C = "ANOTHER_CONFLICTED#{DATA1}"
+
+      Dir.chdir @repos[:cr]
+      %x(
+        echo -e "#{NEWDATA1_PREF_C}" > #{FILE1}
+        git add "#{FILE1}"
+        git commit -am "commit for conflict 1"
+      )
+
+      Dir.chdir @repos[:cr2]
+      %x(
+        echo -e "#{NEWDATA1_SUFF_C}" > #{FILE1}
+        git add "#{FILE1}"
+        git commit -am "commit for conflict 2"
+      )
+
+      expect(File.read(@repos[:cr]+"/"+FILE1).strip).to eq NEWDATA1_PREF_C
+      expect(File.read(@repos[:cr]+"/"+FILE2).strip).to eq DATA2
+
+      expect(File.read(@repos[:cr2]+"/"+FILE1).strip).to eq NEWDATA1_SUFF_C
+      expect(File.read(@repos[:cr2]+"/"+FILE2).strip).to eq DATA2
+
+      Dir.chdir @repos[:cr2]
+
+      %x(
+        git pull
+      )
+
+      conflict = %x(
+        git status | grep "both modified"
+        echo $?
+      ).split("\n").last.strip == "0"
+
+      expect(conflict).to eq true
+
+      %x(
+        git checkout --theirs file1
+        git add file1
+        git commit -m "take other changes"
+      )
+
+      expect(File.read(@repos[:cr2]+"/"+FILE1).strip).to eq File.read(@repos[:cr]+"/"+FILE1).strip
+    end
+
+  end
 end
 
